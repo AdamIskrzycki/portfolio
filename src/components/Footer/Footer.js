115e600b1b3a0a78118b6f7c2aab749fb1162acf
AdamIskrzycki <aiskrzycki01@gmail.com>
2022-09-26 11:54:03 +0200

    Made page responsive, ProjectTile background on mobile WIP

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,12 +8,9 @@ const Footer = (props) => {
     return <section className={classes.Footer}>
         <div className={classes.FeelFree}>FEEL FREE TO CONTACT ME!</div>
         <div className={classes.ContactInfo}>
-            <div classNeme={classes.Icon}><MailIcon fontSize='inherit'/></div>
-            <div classNeme={classes.Icon}><LinkedInIcon fontSize='inherit'/></div>
-            <div classNeme={classes.Icon}><FacebookIcon fontSize='inherit'/></div>
-        </div>
-        <div className={classes.GitHub}>
-
+            <div className={classes.Icon}><a href="mailto:aiskrzycki01@gmail.com" target="_blank"><MailIcon fontSize='inherit' style={{ color: 'ff9b2a' }}/></a></div>
+            <div className={classes.Icon}><a href="https://www.linkedin.com/in/adam-iskrzycki-3884881b2/" target="_blank"><LinkedInIcon fontSize='inherit' style={{ color: 'ff9b2a' }}/></a></div>
+            <div className={classes.Icon}><a href="https://www.facebook.com/adam.iskrzycki.3/" target="_blank"><FacebookIcon fontSize='inherit' style={{ color: 'ff9b2a' }}/></a></div>
         </div>
     </section>
 }
--- src/components/ProjectsGalery/ProjectTile/ProjectTile.js
+++ src/components/ProjectsGalery/ProjectTile/ProjectTile.js
@@ -17,17 +17,21 @@ const ProjectTile = (props) => {
         { backgroundImage: `url(/${props.imageURL})` }
     }>
         <div className={isInfoVisible ? classes.ProjectInfoVisible : classes.ProjectInfoHidden}>
-            <header className={classes.Title}>{props.title}</header>
-            <article className={classes.Description}>{props.description}</article>
-            <section className={classes.Tools}>{props.tools.map(el => {
-               return <div className={classes.Tool}>{el}</div>
-            })}</section>
-            <section className={classes.DemoURL}>{
-            <a href={props.demoURL} target="_blank"><OpenInBrowserIcon fontSize="inherit" style={{ color: 'ff9b2a' }}></OpenInBrowserIcon></a>
-            }</section>
-            <section className={classes.GithubRepoURL}>{
-            <a href={props.githubRepoURL} target="_blank"><GitHubIcon fontSize="inherit" style={{ color: 'ff9b2a' }}></GitHubIcon></a>
-            }</section>
+            <div className={classes.ProjectDetailsContainer}>
+                <header className={classes.Title}>{props.title}</header>
+                <article className={classes.Description}>{props.description}</article>
+                <section className={classes.Tools}>{props.tools.map(el => {
+                return <div className={classes.Tool}>{el}</div>
+                })}</section>
+            </div>
+            <div className={classes.ProjectLinksContainer}>
+                <section className={classes.DemoURL}>{
+                <a href={props.demoURL} target="_blank"><OpenInBrowserIcon fontSize="inherit" style={{ color: 'ff9b2a' }}></OpenInBrowserIcon></a>
+                }</section>
+                <section className={classes.GithubRepoURL}>{
+                <a href={props.githubRepoURL} target="_blank"><GitHubIcon fontSize="inherit" style={{ color: 'ff9b2a' }}></GitHubIcon></a>
+                }</section>
+            </div>
         </div>
     </div>
 }
